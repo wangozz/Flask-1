a1e5e4cd8f18898f3fbcf512c82b5cdc76496803
SamuelWango <wangos478@gmail.com>
2023-09-25 10:49:10 +0300

    under dev

--- models/restaurant.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class Restaurant(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
-    address = db.Column(db.String(255), nullable=False)
-
-    def __repr__(self):
-        return f'<Restaurant {self.name}>'
